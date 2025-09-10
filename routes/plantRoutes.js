343cff71437faf8446a49d6e5393ac7a1791bb2f
Sanskar satale <sanskarsatale186@gmail.com>
2025-09-10 17:27:51 +0530

    new file added

---
+++ routes/plantRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const Plant = require("../models/plant");
+const router = express.Router();
+
+// Get all plants
+router.get("/", async (req, res) => {
+  try {
+    const plants = await Plant.find();
+    res.json(plants);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Add a new plant
+router.post("/", async (req, res) => {
+  try {
+    const newPlant = new Plant(req.body);
+    const savedPlant = await newPlant.save();
+    res.status(201).json(savedPlant);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+module.exports = router;

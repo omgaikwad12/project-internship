938a15478627e1e4d099216ccef24220cbc6dc3e
[om gaikwad] <[omgaikwad1318@gmail.com]>
2025-09-21 18:18:47 +0530

    chatbot added

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
+
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 require("dotenv").config();
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
-app.get("/", (req, res) => {
-res.send("🌱 Greenopedia backend is running!");
 
+app.get("/", (req, res) => {
+  res.send("🌱 Greenopedia backend is running!");
 });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-console.log(`✅ Greenopedia backend running on http://localhost:${PORT}`);
+  console.log(`✅ Greenopedia backend running on http://localhost:${PORT}`);
 });
